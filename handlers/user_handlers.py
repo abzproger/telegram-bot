4575e8537c5def9c1625b726bc33654808638066
Abzaydin <abzaydin.1@gmail.com>
2023-09-24 02:05:33 +0300

    V.031 Добавление пагинации

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,8 +1,8 @@
 from aiogram import Router
 from aiogram import F
-from aiogram.filters import Command, CommandStart
+from aiogram.filters import Command, CommandStart,Filter
 from aiogram.types import Message
-from lexicon.lexicon_ru import LEXICON,FSM_LEXICON
+from lexicon.lexicon_ru import LEXICON, FSM_LEXICON, pagination
 from database.database import Products, db, user, Cart
 from aiogram.filters.state import State, StatesGroup, StateFilter
 from aiogram.fsm.context import FSMContext
@@ -23,8 +23,17 @@ class Pagination:
     count_pages: int = 0
     quantity: int = 1
     current_page :int =None
-    current_cat :str =None
+    product_data : list =None
 
+class Cats_Filter(Filter):
+    def __init__(self, categories:list) -> None:
+        self.my_cats = categories
+
+    async def __call__(self, message: Message) -> bool:
+        return message.text in self.my_cats
+
+
+categories = list(set([x[0] for x in product.get_categories()]))
 
 class FSMFillForm(StatesGroup):
     fill_name = State()
@@ -88,10 +97,26 @@ async def get_location(message: Message):
 
 @router.message(F.text == (LEXICON['start_buy']))
 async def start_buy(message:Message):
-    cats =set( [x[0] for x in product.get_categories()])
-    await message.answer(text=LEXICON['select_cats'],reply_markup=kb_generator(cats))
+    await message.answer(text=LEXICON['select_cats'],reply_markup=kb_generator(categories))
+    print(message.from_user.id)
 
-@router.message()
+#Тут исправить
+@router.message(Cats_Filter(categories=categories))
 async def get_products_in_categories(message:Message):
-    products = [x for x in product.get_products(category=message.text)][0]
-    await message.answer_photo(photo=products[-1],caption=f'<i><b>{products[1]}</b></i>\n<b>Хар-ки:</b> <i>{products[3]}</i>\n<b>Категория:</b> <i>{products[2]}</i>\n<b>Стоимость:</b> <i>{products[4]}</i>')
+    data: list = [x for x in product.get_products(category=message.text)[Pagination.count_pages]]
+    Pagination.product_data = data
+    await message.answer_photo(photo=data[-1],caption=f"<strong>{data[1]}</strong>\n<b>Характеристики:</b>\n{data[3]}\n<b>Категория:</b> {data[2]}\n<b>Стоимость:</b> {data[4]} руб.\n<i>ID:</i> {data[0]}",reply_markup=ikb_generator(2,pagination[0],pagination[1]))
+
+@router.callback_query(F.data == pagination[1])
+async def pag_next(callback:CallbackQuery):
+    Pagination.count_pages += 1
+    data = Pagination.product_data
+    try:
+        if Pagination.count_pages < len(product.get_products()) - 1:
+            await callback.message.answer_photo(photo=data[-1],caption=f"<strong>{data[1]}</strong>\n<b>Характеристики:</b>\n{data[3]}\n<b>Категория:</b> {data[2]}\n<b>Стоимость:</b> {data[4]} руб.\n<i>ID:</i> {data[0]}",reply_markup=ikb_generator(2,pagination[0],pagination[1]))
+        else:
+            await callback.answer(text=LEXICON['error'])
+    except TelegramBadRequest:
+        await callback.answer()
+
+
